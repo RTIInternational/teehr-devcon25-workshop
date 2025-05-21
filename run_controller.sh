bdf650a05d32317e1742ec9b2a2d0d15c7df5705
samlamont <sam.lamont@gmail.com>
2025-05-21 08:55:36 -0400

    Hide notebook latest news message

--- run_controller.sh
+++ run_controller.sh
@@ -3,6 +3,7 @@ if [ "$1" == "run_teehr" ]; then
     # jupyter nbconvert --to notebook --execute /app/notebooks/01_TEEHR_NGIAB.ipynb --inplace
     papermill /app/notebooks/01_TEEHR_NGIAB.ipynb /app/notebooks/01_TEEHR_NGIAB.ipynb --progress-bar --log-output --log-level INFO --cwd /app/notebooks
 elif [ "$1" == "run_jupyter" ]; then
+    jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
     jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --notebook-dir=/app/notebooks --no-browser --NotebookApp.token='' --NotebookApp.password=''
 else
     echo "Error: Invalid argument."
